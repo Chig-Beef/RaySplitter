2044a7b06431cf98b21e346a521e6768f0bf1c53
Chig-Beef <rhystobyharris@gmail.com>
2026-06-03 09:43:03 +0930

    Create arg parsers

--- TestScreen/Commands.c
+++ TestScreen/Commands.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "Commands.h"
 #include "../RaySplitter/RaySplitter.h"
@@ -36,6 +38,82 @@ Color argToColor(char *arg) {
   ((unsigned char*)&c)[n++] = atoi(temp);
 
   return c;
+}
+
+float argToFloat(char *arg) {
+  return atof(arg);
+}
+
+char *argToString(char *arg) {
+  const int len = strlen(arg);
+  char *out = malloc(len+1);
+  memcpy(out, arg, len);
+  out[len] = 0;
+  return out;
+}
+
+Vector2 argToVector2(char *arg) {
+  Vector2 v;
+
+  char temp[10];
+  
+  int i = 0;
+  char *p = arg;
+  int n = 0;
+
+  while (*p) {
+    if (*p == '-') {
+      // End
+      temp[i] = 0;
+
+      // TODO: Support float vector2s
+      ((float*)&v)[n++] = (float)atoi(temp);
+
+      i = 0;
+    } else {
+      temp[i++] = *p;
+    }
+    
+    ++p;
+  }
+
+  // End
+  temp[i] = 0;
+  ((float*)&v)[n++] = (float)atoi(temp);
+
+  return v;
+}
+
+Rectangle argToRectangle(char *arg) {
+  Rectangle r;
+
+  char temp[10];
+  
+  int i = 0;
+  char *p = arg;
+  int n = 0;
+
+  while (*p) {
+    if (*p == '-') {
+      // End
+      temp[i] = 0;
+
+      // TODO: Support float rects over wire
+      ((float*)&r)[n++] = (float)atoi(temp);
+
+      i = 0;
+    } else {
+      temp[i++] = *p;
+    }
+    
+    ++p;
+  }
+
+  // End
+  temp[i] = 0;
+  ((float*)&r)[n++] = (float)atoi(temp);
+
+  return r;
 }
 
 void drawRectangleWrapper(int argc, char **argv) {
--- TestScreen/Commands.h
+++ TestScreen/Commands.h
@@ -3,9 +3,13 @@
 
 #include "../RaySplitter/winray.h"
 
+// Converters into actual types
 int argToInt(char *arg);
-
 Color argToColor(char *arg);
+float argToFloat(char *arg);
+char *argToString(char *arg);
+Vector2 argToVector2(char *arg);
+Rectangle argToRectangle(char *arg);
 
 void drawRectangleWrapper(int argc, char **argv);
 
